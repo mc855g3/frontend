52bdfa92f3a85f84b74c01ceec4362242b496f1c
Thales Rogério <thalesrogerio@gmail.com>
2022-07-13 20:56:10 -0300

    begin geolocalization implementation

--- src/components/inputHC.js
+++ src/components/inputHC.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { API_BASE } from '../constants/constants';
 const InputHC = (attrs) => {
 
+  const [lat,setLat] = useState()
+  const [long,setLong] = useState()
+
+
   const { register, handleSubmit } = useForm();
 
   const checkAccept = async (data) => {
+
+    navigator.geolocation.getCurrentPosition(
+      function(position) {
+        console.log(position);
+        const form_data = new FormData()
+
+        form_data.append("hc",data.hc_input)
+        
+        console.log(position.coords.latitude,position.coords.latitude)
+        const response= fetch(`${API_BASE}/pacient`, {
+          method: "POST",
+          body: form_data,
+        })
+        attrs.updateFunction(attrs.stateUponSubmit)
+      },function(error) {
+        alert("Nos verificamos sua localização para verificar se você realmente esta nas proximidades do HC. Você precisara liberar a permissão de geolocalização para continuar.");
+      }
+    )
     // TODO: make api request
-    const form_data = new FormData()
+    // const form_data = new FormData()
 
-    form_data.append("hc",data.hc_input)
+    // form_data.append("hc",data.hc_input)
 
-    const response= await fetch(`${API_BASE}/pacient`, {
-      method: "POST",
-      body: form_data,
-    })
-    console.log(response.json())
-    attrs.updateFunction(attrs.stateUponSubmit)
+    // const response= await fetch(`${API_BASE}/pacient`, {
+    //   method: "POST",
+    //   body: form_data,
+    // })
+    // attrs.updateFunction(attrs.stateUponSubmit)
   }
+
+  useEffect(()=>{
+    
+  })
+
   return (
     <InputForm onSubmit={handleSubmit(checkAccept)}>
         <InputComponent required placeholder='Seu HC' {...register('hc_input')}/>
